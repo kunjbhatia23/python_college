4cff07fbfaa63a75abb03f2e6fd36930f6eff334
Kunj Bhatia <kunjbhatia23@gmail.com>
2025-01-23 15:51:02 +0530

    Q6 Done

--- AssQ6.py
+++ AssQ6.py
@@ -1,1 +1,6 @@
 #Print all integers that are not divisible by either 2 or 3 and lie between 1 and 50
+
+print("Numbers Are - ")
+for i in range(1, 51):
+    if i % 2 != 0 and i % 3 != 0:  
+        print(i)  
