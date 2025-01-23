b638cea0a578eb8effc7bf6565ac0339740dc5d2
Kunj Bhatia <kunjbhatia23@gmail.com>
2025-01-23 14:49:16 +0530

    Second Commit

--- AssgQ2.py
+++ AssgQ2.py
@@ -1,4 +1,5 @@
 #Print all numbers in a range divisible by a given number
 n=int(input("Enter Number - ")) 
-for i in range (1,10):
-    print(n)
+for i in range (1,n+1):
+    if(n%i) == 0:
+        print(i)
